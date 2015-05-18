c39f954b984c18469da67ac778a0f74871dc8b75
toadums <pademchuk@gmail.com>
2015-05-18 15:00:04 -0700

    add scss lint

--- install.sh
+++ install.sh
@@ -10,10 +10,11 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 echo "Copying over config files"
-ln -s ./.dotfiles/vimrc        ~/.vimrc
-ln -s ./.dotfiles/bash_prompt  ~/.bash_prompt
-ln -s ./.dotfiles/bashrc       ~/.bashrc
-ln -s ./.dotfiles/bash_profile ~/.bash_profile
+ln -s ./.dotfiles/vimrc             ~/.vimrc
+ln -s ./.dotfiles/bash_prompt       ~/.bash_prompt
+ln -s ./.dotfiles/bashrc            ~/.bashrc
+ln -s ./.dotfiles/bash_profile      ~/.bash_profile
 
-ln -s ./.dotfiles/gitconfig    ~/.gitconfig
-ln -s ./.dotfiles/agignore     ~/.agignore
+ln -s ./.dotfiles/gitconfig         ~/.gitconfig
+ln -s ./.dotfiles/agignore          ~/.agignore
+ln -s ./.dotfiles/scss-lint.yml     ~/.scss-lint.yml
