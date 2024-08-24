99e2ce494e16715289cf4230e2509375ead35a05
toadums <pademchuk@gmail.com>
2024-08-23 18:36:17 -0700

    update

--- fish/config.fish
+++ fish/config.fish
@@ -18,10 +18,12 @@ fish_add_path /opt/homebrew/opt/ruby/bin
 
 alias vi="nvim"
 
+alias tree-all="tree -I 'node_modules' -I  'dist'"
+
 set -x ANDROID_SDK_ROOT $HOME/Library/Android/sdk
 
 set PATH /usr/local/bin $PATH
-set PATH /Applications/Postgres.app/Contents/Versions/latest/bin $PATH
+set PATH /Applications/Postgres.app $PATH
 set PATH $HOME/n/bin $PATH
 set PATH $ANDROID_SDK_ROOT/emulator $PATH
 set PATH $ANDROID_SDK_ROOT/platform-tools $PATH
@@ -32,3 +34,5 @@ set -Ux JAVA_HOME $(/usr/libexec/java_home)
 set -Ux N_PREFIX $HOME/n/
 
 status --is-interactive; and rbenv init - fish | source
+
+alias repo="set url $(git remote get-url origin 2>/dev/null | sed 's/git@/https:\/\//' | sed 's/com:/com\//');[ -z \"\$url\" ] && set url 'https://github.com'; open \"\$url\""
